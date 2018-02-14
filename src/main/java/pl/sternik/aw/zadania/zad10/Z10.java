f1174ac3aa9633a76e8906b857c33db8a79fcbea
Andrzej <andrzejwasilkowski@gmail.com>
2018-02-14 19:22:38 +0100

    Zad 10

---
+++ src/main/java/pl/sternik/aw/zadania/zad10/Z10.java
@@ -0,0 +1,52 @@
+package pl.sternik.aw.zadania.zad10;
+
+public class Z10 {
+    public static void main(String[] args){
+        int[][] liczby = {{2,3,4,5},{5,4,3,2}};
+        String[] znaki = {"+","-","*","/"};
+
+        Z10 z10 = new Z10();
+
+        int[] wynik = z10.oblicz(liczby,znaki);
+        wypisz(wynik);
+    }
+    public static void wypisz(int[] tab){
+        for (int i:tab){
+            System.out.print(i+" ");
+        }
+    }
+
+    public int[] oblicz(int[][] liczby, String[] znaki){
+        int[] wynik = new int[znaki.length];
+        int wybor;
+
+        for (int i=0;i<znaki.length;i++){
+            if(znaki[i]=="*"){
+                wybor = 1;
+            } else if(znaki[i]=="/"){
+                wybor = 2;
+            } else if(znaki[i]=="-") {
+                wybor = 3;
+            } else {
+                wybor = 4;
+            }
+
+            switch (wybor){
+                case 1:
+                    wynik[i] = liczby[0][i] * liczby[1][i];
+                    break;
+                case 2:
+                    wynik[i] = liczby[0][i] / liczby[1][i];
+                    break;
+                case 3:
+                    wynik[i] = liczby[0][i] - liczby[1][i];
+                    break;
+                case 4:
+                    wynik[i] = liczby[0][i] + liczby[1][i];
+                    break;
+            }
+
+        }
+        return wynik;
+    }
+}
---
+++ src/test/java/pl/sternik/aw/zadania/zad10/Z10Test.java
@@ -0,0 +1,21 @@
+package pl.sternik.aw.zadania.zad10;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Z10Test {
+
+    @Test
+    public void oblicz() {
+        int[][] liczby = {{10,20,30,40},{5,4,3,2}};
+        String[] znaki = {"+","-","*","/"};
+
+        int[] oczekiwany = {15,16,90,20};
+
+        Z10 z10 = new Z10();
+
+        int[] wynik = z10.oblicz(liczby,znaki);
+        assertThat(wynik).isEqualTo(oczekiwany);
+    }
+}

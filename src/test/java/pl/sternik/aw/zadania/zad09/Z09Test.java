ac3df777d8d6029a958d8eaf6bc0c260733b7a03
Andrzej <andrzejwasilkowski@gmail.com>
2018-02-14 18:54:27 +0100

    Zad 9

---
+++ src/test/java/pl/sternik/aw/zadania/zad09/Z09Test.java
@@ -0,0 +1,19 @@
+package pl.sternik.aw.zadania.zad09;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Z09Test {
+
+    @Test
+    public void zamien() {
+        int[][] tab = {{2,3,4},{4,3,2,6},{7,5,6,8,7},{3,4,2}};
+        int[][] tab1 = tab.clone();
+        int w1 = 1;
+        int w2 = 2;
+        Z09 z09 = new Z09();
+        z09.zamien(w1,w2,tab);
+        assertThat(tab[w1]).isEqualTo(tab1[w2]);
+    }
+}

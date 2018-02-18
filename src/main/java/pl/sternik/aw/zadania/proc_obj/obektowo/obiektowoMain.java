da57052631d599f92ac48bd3bd60a693567cd2f4
Andrzej <andrzejwasilkowski@gmail.com>
2018-02-18 13:48:49 +0100

    proc-obj brakujący plik

--- src/main/java/pl/sternik/aw/zadania/proc_obj/obektowo/obiektowoMain.java
+++ src/main/java/pl/sternik/aw/zadania/proc_obj/obektowo/obiektowoMain.java
@@ -1,4 +1,20 @@
 package pl.sternik.aw.zadania.proc_obj.obektowo;
 
 public class obiektowoMain {
+    public static void main(String[] args) {
+        Figura f = new Prostokat(2, 4);
+        System.out.print("Prostokąt pole i obwód: ");
+        System.out.print(f.obliczObwod() + "  ");
+        System.out.println(f.obliczObwod());
+
+        f = new Kwadrat(2);
+        System.out.print("Kwadrat pole i obwód: ");
+        System.out.print(f.obliczObwod() + "  ");
+        System.out.println(f.obliczObwod());
+
+        f = new Kolo(4);
+        System.out.print("Koło pole i obwód: ");
+        System.out.print(f.obliczObwod() + "  ");
+        System.out.println(f.obliczObwod());
+    }
 }

905b7e3a7b1c110f8207e4f8ee98c802b1423a70
Andrzej <andrzejwasilkowski@gmail.com>
2018-02-25 22:05:55 +0100

    #2 Obsłużone dzielenie przez 0, a co do testów to nie jestem pewny czy tak może to być

--- src/main/java/pl/sternik/aw/zadania/zad10/Z10.java
+++ src/main/java/pl/sternik/aw/zadania/zad10/Z10.java
@@ -2,11 +2,9 @@ package pl.sternik.aw.zadania.zad10;
 
 public class Z10 {
     public static void main(String[] args){
-        int[][] liczby = {{2,3,4,5},{5,4,3,2}};
+        int[][] liczby = {{2,3,4,5},{5,4,3,1}};
         String[] znaki = {"+","-","*","/"};
-
         Z10 z10 = new Z10();
-
         int[] wynik = z10.oblicz(liczby,znaki);
         wypisz(wynik);
     }
@@ -36,7 +34,23 @@ public class Z10 {
                     wynik[i] = liczby[0][i] * liczby[1][i];
                     break;
                 case 2:
-                    wynik[i] = liczby[0][i] / liczby[1][i];
+                    try {
+                        wynik[i] = liczby[0][i] / liczby[1][i];
+                    } catch (ArithmeticException e){
+                        throw e;
+                    }
+                    /* Można założyć, że jak ktoś planuje dzielić przez 0 to to jakby chciał przez 1. Zawsze jakieś rozwiązanie.
+                    if(liczby[1][i]!=0){
+                        wynik[i] = liczby[0][i] / liczby[1][i];
+                    } else {
+                        wynik[i] = liczby[0][i] / 1;
+                    }
+                    */
+                    /* Albo po prostu pomijamy to działanie i w tym miejscu będzie 0
+                    if(liczby[1][i]!=0){
+                        wynik[i] = liczby[0][i] / liczby[1][i];
+                    }
+                    */
                     break;
                 case 3:
                     wynik[i] = liczby[0][i] - liczby[1][i];
--- src/test/java/pl/sternik/aw/zadania/zad10/Z10Test.java
+++ src/test/java/pl/sternik/aw/zadania/zad10/Z10Test.java
@@ -10,7 +10,18 @@ public class Z10Test {
     public void oblicz() {
         int[][] liczby = {{10,20,30,40},{5,4,3,2}};
         String[] znaki = {"+","-","*","/"};
+        int[] oczekiwany = {15,16,90,20};
 
+        Z10 z10 = new Z10();
+
+        int[] wynik = z10.oblicz(liczby,znaki);
+        assertThat(wynik).isEqualTo(oczekiwany);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void obliczDzieleniePrzezZero() {
+        int[][] liczby = {{10,20,30,40},{5,4,3,0}};
+        String[] znaki = {"+","-","*","/"};
         int[] oczekiwany = {15,16,90,20};
 
         Z10 z10 = new Z10();

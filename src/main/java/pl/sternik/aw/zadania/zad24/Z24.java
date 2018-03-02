ba3ad7dba8a0010031c88924bf26c98a30160dff
Andrzej <andrzejwasilkowski@gmail.com>
2018-03-02 20:43:38 +0100

    Zad24

--- src/main/java/pl/sternik/aw/zadania/zad24/Z24.java
+++ src/main/java/pl/sternik/aw/zadania/zad24/Z24.java
@@ -1,5 +1,7 @@
 package pl.sternik.aw.zadania.zad24;
 
+import pl.sternik.aw.zadania.zad07.DivideByZeroException;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
@@ -9,26 +11,38 @@ public class Z24 {
     private int[] tab = {1,2,3,4};
 
     public static void main(String[] args) {
-
         Z24 z24 = new Z24();
-        z24.podziel(1,0);
+        int x = 1;
+        int y = 0;
+        try {
+            if(y == 0) {
+                throw new DivideByZeroException("Dzielenie przez 0!!!!");
+            }
+            z24.podziel(x, y);
+        } catch (DivideByZeroException e){
+            System.out.println("Tak nie wolno!");
+            e.printStackTrace();
+        }
+        System.out.println();
+        System.out.println(z24.dajElement(4));
 
+        System.out.println();
+        System.out.println(z24.wczytaj());
 
     }
 
-    public double podziel(int i, int i1) {
-        double wynik = i / i1;
+    public double podziel(int i, int i1) throws ArithmeticException {
+        double wynik = 0;
+        wynik = i / i1;
         return wynik;
     }
 
-    public int dajElement(int i) {
+    public int dajElement(int i){
         try {
-            System.out.println(tab[i]);
             return tab[i];
-        } catch (IndexOutOfBoundsException e){
-            System.out.println("Błąd indexu");
-            System.out.println(e);
-            return -1;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("IndexOutOfBoundsException. Nie ma tekiego indexu: " + i);
+            return 0;
         }
     }
     public static Date wczytaj() {
@@ -44,6 +58,7 @@ public class Z24 {
             System.out.println("Błąd:" + e);
             return null;
         } finally {
+            System.out.println("Wywołanie finally");
             in.close();
         }
     }
--- src/test/java/pl/sternik/aw/zadania/zad24/Z24Test.java
+++ src/test/java/pl/sternik/aw/zadania/zad24/Z24Test.java
@@ -3,18 +3,22 @@ package pl.sternik.aw.zadania.zad24;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+import pl.sternik.aw.zadania.zad07.DivideByZeroException;
 
 public class Z24Test {
-    @Test(expected = ArithmeticException.class)
-    public void testDzieleniePrzez0() throws Exception {
-        Z24 sut = new Z24();
+    private Z24 z24 = new Z24();
+    private int x = 1;
+    private int y = 0;
+    private int[] tab = {1,2,3,4};
+    private int index = 4;
 
-        sut.podziel(1, 0);
+    @Test(expected = ArithmeticException.class)
+    public void testDzieleniePrzezZero() throws ArithmeticException {
+        z24.podziel(x, y);
     }
 
     @Test
-    public void testDajElement() throws Exception {
-        //brak testu
+    public void testDajElementOutOfBoundException() {
+        assertThat(z24.dajElement(index)).isEqualTo(0);
     }
-
 }
